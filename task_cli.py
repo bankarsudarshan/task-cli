acf9c6968e172c6f05eb4c16a902777fa9fe4bb8
Sudarshan Bankar <sudarshanbankar22@gmail.com>
2025-10-16 12:27:56 +0530

    update functionality added

--- task_cli.py
+++ task_cli.py
@@ -5,13 +5,10 @@ implementation of task-cli
 """
 
 import os
+import sys
 import argparse
 import json
 from datetime import datetime
-
-class UpdateAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, dict(task_id=int(values[0]), task_name=values[1]))
 
 class Operations:
     def __init__(self, file):
@@ -34,6 +31,10 @@ class Operations:
 
         json_file = new_id = result = None
 
+        if not os.path.isfile(self.filename):
+            with open(self.filename, 'w') as f:
+                pass
+        
         if os.stat(self.filename).st_size == 0:
             json_file = {}
             new_id = 1
@@ -43,24 +44,55 @@ class Operations:
                 new_id = int(max(json_file)) + 1
         json_file[new_id] = new_task
         with open(self.filename, 'w', encoding='utf-8') as f:
-            result = json.dump(json_file, f, indent="\t")
+            json.dump(json_file, f, indent="\t")
+        return new_id
 
-        print(f"task added successfully")
+    def update(self, args):
+        """
+        1. Whichever of arg1 or arg2 is integer, assign it to 'id' and assign the other to 'new_desc'.. But one of arg1, arg2 has to be integer
+        2. get all the tasks out using json.load()
+        3. if task with 'id' is present, then change its "description" to 'new_desc'
+        4. else show a command line error
+        """
+        id = new_desc = None
+        if args[0].isnumeric() == args[1].isnumeric():
+            # if both args are numeric or both are alpha, then they are incorrect arguments
+            raise ValueError("one argument is supposed to be an int while other, a new name for a task")
+        if args[0].isnumeric():
+            id, new_desc = args[0], args[1]
+        else:
+            id, new_desc = args[1], args[0]
         
+        tasks = None
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as f:
+                tasks = json.load(f)
+        except FileNotFoundError:
+            print(f"No task with (ID:{id})")
+            sys.exit(1) # terminate the program here
+
+        if id in tasks:
+            tasks[id]['description'] = new_desc
+        else:
+            raise ValueError(f"No task with (ID:{id})")
+        
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            json.dump(tasks, f, indent="\t")
+        return id
 
 parser = argparse.ArgumentParser() # the ArgumentParser object
 
-parser.add_argument("operation", type=str, help="operation to perform, for eg, add/delete/update/list")
-parser.add_argument("task_arg1", help="takes integer if the 'operation' argument is update/delete/mark-done/mark-in-progress else string")
-parser.add_argument("--task_arg2")
+parser.add_argument("--add", "-a", type=str)
+parser.add_argument("--update", "-u", dest="update", nargs=2)
+parser.add_argument("--delete", "-d", nargs=1)
 args = parser.parse_args() # the Namespace object, returned by parse_args()
 
 filename = "tasks.json"
 ops = Operations(filename)
 
-match args.operation:
-    case "add" | "a":
-        result = ops.add(args.task_arg1)
-        print(f"task added in the json file successfully; return value = {result}")
-    case "update" | "u":
-        pass
+if args.add:
+    id = ops.add(args.add)
+    print(f"Task added successfully (ID:{id})")
+if args.update:
+    result = ops.update(args.update)
+    print(f"task with {result} updated successfully")
