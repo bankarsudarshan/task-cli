2ccc0cae66095749009c661c0a2245cf9aada6da
Sudarshan Bankar <sudarshanbankar22@gmail.com>
2025-10-22 09:30:24 +0530

    created the add task functionality. This time using the sub-commands feature of argparse

---
+++ task_cli.py
@@ -0,0 +1,62 @@
+from argparse import ArgumentParser
+import json
+from datetime import datetime
+import os
+
+filename = 'tasks.json'
+
+def load_file(filename):
+    if not os.path.isfile(filename):
+        with open(filename, 'w', encoding='utf-8'):
+            pass
+        return {}
+    
+    elif os.stat(filename).st_size == 0:
+        return {}
+    
+    tasks = None
+    with open(filename, 'r', encoding='utf-8') as f:
+        tasks = json.load(f)
+    
+    return tasks
+
+def save_file(tasks, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(tasks, f, indent="\t")
+
+def add(args):
+    new_task = {
+        "description": args.task,
+        "status": "to-do",
+        "createdAt": str(datetime.now()),
+        "updatedAt": str(datetime.now())
+    }
+    tasks = load_file(filename)
+    id = -1
+    if len(tasks) == 0:
+        id = 1
+    else:
+        id = int(max(tasks)) + 1
+
+    tasks[id] = new_task
+    save_file(tasks, filename)
+    return id
+
+def update(args):
+    print("hi in update()")
+    print(f"update subparser arguments = {args}")
+
+parser = ArgumentParser()
+subparsers = parser.add_subparsers()
+
+parser_add = subparsers.add_parser('add')
+parser_add.add_argument('task', type=str)
+parser_add.set_defaults(func=add)
+
+parser_update = subparsers.add_parser('update')
+parser_update.add_argument('id', type=int)
+parser_update.add_argument('new_desc', type=str)
+parser_update.set_defaults(func=update)
+
+args = parser.parse_args()
+args.func(args)
