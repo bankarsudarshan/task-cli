49b54d08d780a2c2edafad1b87c33b4e67e864ef
Sudarshan Bankar <sudarshanbankar22@gmail.com>
2026-01-24 19:36:57 +0530

    list command connect to services successfully

--- src/taskman/commands/list_cmd.py
+++ src/taskman/commands/list_cmd.py
@@ -1,4 +1,8 @@
-def register(subparser, list_tasks):
+from taskman.core.services import CLIService
+from taskman.utils import render_tasks_table
+
+
+def register(subparser, service: CLIService):
     parser_list = subparser.add_parser(
         "list",
         help="List tasks",
@@ -35,49 +39,15 @@ def register(subparser, list_tasks):
         default="asc",
         help="Sort order (default: asc)",
     )
-    parser_list.set_defaults(func=list_tasks)
+    parser_list.set_defaults(func=lambda args: run(args, service))
 
 
-# list_subparsers = parser_list.add_subparsers(
-#     dest="l2_subparser_list",
-#     description="More granular commands on top of list command",
-# )
-
-# parser_list_archived = list_subparsers.add_parser(
-#     "archived",
-#     help="List done tasks",
-#     description="List done / archived tasks",
-# )
-# parser_list_archived.add_argument(
-#     "tasks_type",
-#     type=str,
-#     choices=["in-progress", "todo", "done", "all"],
-#     default="all",
-#     nargs="?",
-#     help="Filter tasks by status (default: all)",
-# )
-# parser_list_archived.add_argument(
-#     "-p",
-#     "--priority",
-#     type=str,
-#     choices=["low", "medium", "high"],
-#     help="Filter tasks by priority",
-# )
-# parser_list_archived.add_argument(
-#     "-sb",
-#     "--sort-by",
-#     type=str,
-#     choices=["id", "created_at", "updated_at", "priority", "status"],
-#     default="id",
-#     help="Sort tasks by a specific field (default: id)",
-# )
-# parser_list_archived.add_argument(
-#     "-o",
-#     "--order",
-#     type=str,
-#     choices=["asc", "desc"],
-#     default="asc",
-#     help="Sort order (default: asc)",
-# )
-
-# parser_list_archived.set_defaults(func=task.list_tasks)
+def run(args, service: CLIService):
+    filters = {
+        "tasks_type": args.tasks_type,
+        "priority": args.priority,
+        "sort_by": args.sort_by,
+        "order": args.order,
+    }
+    tasks = service.get_tasks(filters)
+    return render_tasks_table(tasks)
